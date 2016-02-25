f8b285feb517afe5636b898e819d67dfff125d33
hkorotkin <hkorotki@vols.utk.edu>
2016-02-25 12:27:30 -0500

    still working on some of these functions

--- CorrelationFunctions.R
+++ CorrelationFunctions.R
@@ -3,16 +3,25 @@ library(geiger) #utilty fns
 library(corHMM)
 library(phylolm)
 library(phytools)
+library(rotl)
+library(phylobase)
 
 VisualizeData <- function(phy, data) {
 	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
 }
 
 CleanData <- function(phy, data) {
+	treedata(phy,data)
 	#treedata() in Geiger is probably my favorite function in R.
 }
 
+
 RunContrasts <- function(phy, data, output.pdf="PIC.pdf") {
+	pic1<-pic(data[,1],phy)
+	pic2<-pic(data[,2],phy)
+	pdf=("PIC.pdf")
+	plot(pic1~pic2)
+	dev.off()
 	#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
 }
 
