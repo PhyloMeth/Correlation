b6b1111aef3b3ebc63f4abfc3936afb8b304da09
Todd Pierson <twpierson@twplaptop2.nomads.utk.edu>
2016-02-25 10:47:22 -0500

    ...annotated things a little.

--- RunCorrelations.R
+++ RunCorrelations.R
@@ -1,6 +1,13 @@
 source("CorrelationFunctions.R")
+
+## Read in a tree for the Eurycea bislineata complex, generated with SNP data. It has branch lengths.
 tree <- read.tree("Eurycea_Tree")
+
+## Read in fake, but plausible discrete data about the presence or absence of 'big heads' and presence or absence of 'bright colors' in these salamanders. I left out one taxa to demonstrate the utility of our 'CleanData' function later on.
 discrete.data <- read.csv(file="Eurycea_Data.csv", stringsAsFactors=FALSE, row.names=1) #death to factors
+
+## Here I'm just totally making up continuous data for SVL (i.e., snout-vent length) and larval period. 
+
 SVL <- rnorm(31,mean=25,sd=8)
 larval.period <- rnorm(31,mean=1.2,sd=0.5)
 continuous.data <- cbind(SVL,larval.period)
