b5dd1b55654fce7fc46702c68435a9e264559620
Todd Pierson <twpierson@twplaptop2.nomads.utk.edu>
2016-02-25 10:31:43 -0500

    ...work in progress

--- CorrelationFunctions.R
+++ CorrelationFunctions.R
@@ -5,19 +5,31 @@ library(phylolm)
 library(phytools)
 
 VisualizeData <- function(phy, data) {
-	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
+	plot(phy)
+	plot(data)
 }
 
 CleanData <- function(phy, data) {
-	#treedata() in Geiger is probably my favorite function in R.
+	treedata(phy,data)
 }
 
 RunContrasts <- function(phy, data, output.pdf="PIC.pdf") {
 	#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
+	pic.1 <- pic(data[,1],phy)
+	pic.2 <- pic(data[,2],phy)
+	pdf("PIC.pdf")
+	plot(pic.2 ~ pic.1 -1, pch=19)
+	dev.off()
+	correlation <- cor.test(pic.1,pic.2)
+	pic.model <- lm(pic.2 ~ pic.1 -1)
+	return(list(correlation,pic.model))
 }
 
 RunPagel94 <- function(phy, data) {
 	#Calculate the rate estimates under each model, the likelihood of the data under each model, and the model averaged rates. 	Return the results
+	
+	fitPagel(phy,dat[[2]][,1],dat[[2]][,2])
+	
 }
 
 RunOtherMethod <- function(phy, data) {
--- RunCorrelations.R
+++ RunCorrelations.R
@@ -1,7 +1,10 @@
 source("CorrelationFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
-continuous.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+tree <- read.tree("Eurycea_Tree")
+discrete.data <- read.csv(file="Eurycea_Data.csv", stringsAsFactors=FALSE, row.names=1) #death to factors
+SVL <- rnorm(31,mean=25,sd=8)
+larval.period <- rnorm(31,mean=1.2,sd=0.5)
+continuous.data <- cbind(SVL,larval.period)
+rownames(continuous.data) <- tree$tip.label
 
 cleaned.continuous <- CleanData(tree, continuous.data)
 cleaned.discrete <- CleanData(tree, discrete.data)
@@ -11,4 +14,3 @@ contrasts.answer <- RunContrasts(tree, continuous.data)
 save(list=ls(), file="CorrelationsResults.rda")
 pagel94.answer <- RunPagel(tree, discrete.data)
 save(list=ls(), file="CorrelationsResults.rda")
-
