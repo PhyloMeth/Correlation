ab1add92acfead08cf411e97271e14d053bbaa2d
Tyson Paulson <TPaulson@vols.utk.edu>
2016-03-04 11:27:50 -0500

    Finished Correlation Exercise

--- CorrelationFunctions.R
+++ CorrelationFunctions.R
@@ -4,21 +4,34 @@ library(corHMM)
 library(phylolm)
 library(phytools)
 
-VisualizeData <- function(phy, data) {
-	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
+VisualizeData <- function(phy, data) {  
+	print(phy)
+	plot(phy)
+	print(data)
+	names(data)
+	str(data)
 }
 
-CleanData <- function(phy, data) {
-	#treedata() in Geiger is probably my favorite function in R.
+#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?	
+
+CleanData <- function(phy, data) { treedata(phy,data,sort=TRUE)
 }
 
-RunContrasts <- function(phy, data, output.pdf="PIC.pdf") {
-	#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
+
+RunContrasts <- function(data, phy, output.pdf="PIC.pdf") {	
+	pic1<-pic(data[,1],phy)
+	pic2<-pic(data[,2],phy)
+	pdf=("PIC.pdf")
+	plot(pic1~pic2)
+	cor.test(pic1,pic2)
 }
 
-RunPagel94 <- function(phy, data) {
-	#Calculate the rate estimates under each model, the likelihood of the data under each model, and the model averaged rates. 	Return the results
-}
+#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
+
+RunPagel94 <- function(phy, data1,data2) { fitPagel(phy,data1,data2)
+	}
+	
+#Calculate the rate estimates under each model, the likelihood of the data under each model, and the model averaged rates. 	Return the results
 
 RunOtherMethod <- function(phy, data) {
 	
--- RunCorrelations.R
+++ RunCorrelations.R
@@ -1,14 +1,26 @@
+set.seed(5)
 source("CorrelationFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
-continuous.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+#setwd("~/desktop/PhyloMeth/Correlation")
+tree <- read.tree("ZanneEtAlChronogram.tre")
+
+dat<-c("Daucus_carota" , "Conyza_canadensis" , "Setaria_viridis" , "Andropogon_gerardii" , "Pueraria_montana" , "Ambrosia_trifida" , "Datura_stramonium" , "Smallanthus_uvedalia" , "Perilla_frutescens" , "Verbesina_jacksonii" , "Vernonia_gigantea" , "Lespedeza_cuneata" , "Ambrosia_artemisiifolia" , "Cichorium_intybus" , "Albizia_julibrissin" , "Cercis_canadensis" , "Sorghum_halepense" , "Sorghastrum_nutans" , "Ligustrum_sinense" , "Gleditsia_triacanthos" , "Microstegium_vimineum" , "Cyperus_strigosus" , "Mosla_chinensis" , "Symphyotrichum_pilosum" , "Erechtites_hieraciifolius" , "Lespedeza_bicolor" , "Helenium_autumnale" , "Trifolium_pratense" , "Acer_saccharum" , "Lonicera_maackii" , "Liriodendron_tulipifera" , "Ailanthus_altissima")
+
+invasive<-c(1,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1)
+woody<-c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,0,1,1,1,1)
+
+discrete.data<-cbind.data.frame(invasive,woody)
+row.names(discrete.data)<-dat
+
+continuous.data <- rnorm(nTips(new.tree$phy),4,1)
+names(continuous.data)<-dat
+
 
 cleaned.continuous <- CleanData(tree, continuous.data)
 cleaned.discrete <- CleanData(tree, discrete.data)
-VisualizeData(tree, cleaned.continuous)
-VisualizeData(tree, cleaned.discrete)
-contrasts.answer <- RunContrasts(tree, continuous.data)
+VisualizeData(cleaned.continuous$phy, cleaned.continuous$data)
+VisualizeData(cleaned.discrete$phy, cleaned.discrete$data)
+contrasts.answer <- RunContrasts(cleaned.discrete$data, cleaned.discrete$phy)
 save(list=ls(), file="CorrelationsResults.rda")
-pagel94.answer <- RunPagel(tree, discrete.data)
+pagel94.answer <- RunPagel94(cleaned.discrete$phy,cleaned.discrete$data[,'woody'], cleaned.discrete$data[,'invasive'])
 save(list=ls(), file="CorrelationsResults.rda")
 
