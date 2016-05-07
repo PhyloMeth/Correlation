2cf0fe8ed7ec1e4ab6ffe476abd3b982230289ed
Ryan Roney <Ryan Roney>
2016-05-07 18:22:16 -0400

    mostly done, issue with matchobj[1, ] : subscript out of bounds in last function

--- CorrelationFunctions.R
+++ CorrelationFunctions.R
@@ -3,30 +3,21 @@ library(geiger) #utilty fns
 library(corHMM)
 library(phylolm)
 library(phytools)
+library(BAMMtools)
 
-discrete.data <- rTraitDisc(tree, model = "ER", k=2, states=c(0,1))
 
-continuous.data <- rTraitCont(tree, model="BM")
 
 
 VisualizeData <- function(phy, data) {
 	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
-  plot.phylo(tree, type = "phylogram", use.edge.length = TRUE,
-             node.pos = NULL, show.tip.label = TRUE, show.node.label = FALSE,
-             edge.color = "black", edge.width = 1, edge.lty = 1, font = 3,
-             cex = par("cex"), adj = NULL, srt = 0, no.margin = FALSE,
-             root.edge = FALSE, label.offset = 0, underscore = FALSE,
-             x.lim = NULL, y.lim = NULL, direction = "rightwards",
-             lab4ut = NULL, tip.color = "black", plot = TRUE,
-             rotate.tree = 0, open.angle = 0, node.depth = 1,
-             align.tip.label = FALSE, ...)
-  plot.phylo(tree, type="fan", cex=0.2)
+  
+  plot.phylo(phy, type="fan", cex=0.2)
   # note to self, don't put the object in quotes
   
-    print(paste("The tree has ", Ntip(tree), " terminals and ", 
-              Nnode(tree), " internal nodes out of ",Ntip(tree)-2,
+    print(paste("The tree has ", Ntip(phy), " terminals and ", 
+              Nnode(phy), " internal nodes out of ",Ntip(phy)-2,
               " possible, which means it is ", 
-              round(100*(Nnode(tree)-1)/(Ntip(tree)-3), 2),
+              round(100*(Nnode(phy)-1)/(Ntip(phy)-3), 2),
               "% resolved", sep=""))
   name.check(phy, data, data.names=NULL)
 }
@@ -38,13 +29,20 @@ CleanData <- function(phy, data) {
 
 RunContrasts <- function(phy, data, output.pdf="PIC.pdf") {
 	#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
-  pic(x, phy, scaled = TRUE, var.contrasts = TRUE, #gives you contrasts and variances
+  pic1 <- pic(data[,1], phy, scaled = TRUE, var.contrasts = TRUE, #gives you contrasts and variances
       rescaled.tree = FALSE
       #need to positivise the contrasts, not just using absolutes, scale data...but is the scaling appropriate. Look at Felsenstein 1985 to understand further.
       # run for each character
       # one loop to positivize
   )
-  pic12 <- cbind(pic1[,1],pic2[,1])
+  pic2 <- pic(data[,2], phy, scaled = TRUE, var.contrasts = TRUE, #gives you contrasts and variances
+                      rescaled.tree = FALSE)
+ 
+  pictest1 <- cor.test(pic1[,1], pic1[,2])
+  pictest2 <- cor.test(pic2[,1], pic2[,2])
+    
+  
+   pic12 <- cbind(pic1[,1],pic2[,1])
   pic1posed <- pic12
   for (i in 1:length(pic1[,1])){
     if (pic1[i,1]<0) {
@@ -53,25 +51,31 @@ RunContrasts <- function(phy, data, output.pdf="PIC.pdf") {
       pic1posed[i,] <- pic12[i,]
     }
   }
-}
+  test <- cor.test(pic1posed[,1], pic1posed[,2])
+  return (list(pic=pic1posed, pictest1=pictest1, pictest2=pictest2, test=test))
+  }
 
 RunPagel94 <- function(phy, data) {
 	#Calculate the rate estimates under each model, the likelihood of the data under each model, and the model averaged rates. 	Return the results
-  corDISC(phy,data, ntraits=2, rate.mat=NULL, model=c("ER","SYM","ARD"), 
-          node.states=c("joint", "marginal", "scaled"), p=NULL, root.p=NULL, ip=NULL, 
-          lb=0, ub=100, diagn=FALSE) #three different models
-}
+  datafixed <- cbind(rownames(data), data[,1], data[,2])
+  PagelER <- corDISC(phy,datafixed, ntraits=2, model=c("ER")) 
+  PagelSYM <- corDISC(phy,datafixed, ntraits=2, model=c("SYM")) 
+  PagelARD <- corDISC(phy,datafixed, ntraits=2, model=c("ARD")) 
+  # corDISC(phy,datafixed, ntraits=2, model=c("ER","SYM","ARD")) #three different models
+  return (list(ER=PagelER, SYM=PagelSYM, ARD=PagelARD))
+  }
 
 RunOtherMethod <- function(phy, data) {
-  corHMM(phy, data, rate.cat, rate.mat=NULL, node.states=c("joint", "marginal","scaled"), 
+  datafixed <- cbind(rownames(data), data[,1], data[,2])
+  Potato <- corHMM(phy, data, rate.cat, rate.mat=NULL, node.states=c("joint"), 
        optim.method=c("subplex"), p=NULL, root.p=NULL, ip=NULL, nstarts=10, n.cores=NULL, 
        sann.its=5000, diagn=FALSE)
-	
+	return (Potato)
 }
 
 
 #Functions I could potentially use:
-phylolm(formula, data = list(), phy, model = c("BM", "OUrandomRoot",
-                                               "OUfixedRoot", "lambda", "kappa", "delta", "EB", "trend"),
-        lower.bound = NULL, upper.bound = NULL, 
-        starting.value = NULL, ...)
+# phylolm(formula, data = list(), phy, model = c("BM", "OUrandomRoot",
+           #                                    "OUfixedRoot", "lambda", "kappa", "delta", "EB", "trend"),
+       # lower.bound = NULL, upper.bound = NULL, 
+        #starting.value = NULL, ...)
--- RunCorrelations.R
+++ RunCorrelations.R
@@ -1,7 +1,18 @@
-source("CorrelationFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
-continuous.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+source("E:/SCHOOL/Documents-School/Class/EEB_603_PhyloMeth/Repository2/Correlation/CorrelationFunctions.R")
+#tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
+# discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+# continuous.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+
+data(whales)
+tree <- whales
+discrete.data1 <- rTraitDisc(tree, model = "ER", k=2, states=c(0,1))
+discrete.data2 <- rTraitDisc(tree, model = "ER", k=2, states=c(0,1))
+discrete.data <- cbind(discrete.data1, discrete.data2)
+
+continuous.data1 <- rTraitCont(tree, model="BM")
+continuous.data2 <- rTraitCont(tree, model="BM")
+continuous.data <- cbind(continuous.data1, continuous.data2)
+
 
 cleaned.continuous <- CleanData(tree, continuous.data)
 cleaned.discrete <- CleanData(tree, discrete.data)
@@ -9,6 +20,6 @@ VisualizeData(tree, cleaned.continuous)
 VisualizeData(tree, cleaned.discrete)
 contrasts.answer <- RunContrasts(tree, continuous.data)
 save(list=ls(), file="CorrelationsResults.rda")
-pagel94.answer <- RunPagel(tree, discrete.data)
+pagel94.answer <- RunPagel94(tree, discrete.data)
 save(list=ls(), file="CorrelationsResults.rda")
-
+OtherMethod.answer <- RunOtherMethod(tree, ??????????.data)
